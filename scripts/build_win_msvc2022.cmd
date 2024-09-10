7d799b3b0b4ef5a69168d78206ea9ae2c9aae7ba
Anton Kurilenko <anton.kurilenko@infovista.com>
2024-09-10 17:19:43 +0300

    Added documentation.
    Ready for release.

--- scripts/build_win_msvc2022.cmd
+++ scripts/build_win_msvc2022.cmd
@@ -1,7 +1,7 @@
 cd .. && mkdir build 
 cd build
 cmake -G "Visual Studio 17 2022" -A x64 -DCMAKE_CONFIGURATION_TYPE=Release .. 
-cmake --build . --config=Release
+cmake --build . --config=Release -j9
 
 cd Release/bin
 test_mpsslib.exe
